8cbde5cc7e82fffbc58081511283a482481ad0cd
Murrx <altenarobin@gmail.com>
2014-11-13 23:37:44 +0100

    initial work on mocking the Persona response

---
+++ accounts/static/accounts.js
@@ -0,0 +1,13 @@
+var initialize = function (navigator, user, token, urls) {
+    $('#id_login').on('click', function () {
+        navigator.id.request();
+    });
+
+    navigator.id.watch({loggedInUser: user});
+};
+
+window.Superlists = {
+    Accounts: {
+        initialize: initialize
+    }
+};
